5e838b6a453982de2fc9f5be6d45bb9f028f33f9
Mahnoor-Khizar <mahnoorkhizar688@gmail.com>
2026-08-19 00:05:09 +0500

    Updated and created 1 file

---
+++ frontend/src/services/familyService.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("virsa_access_token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export async function createFamily(
+  name: string,
+  description: string,
+  isPrivate = true,
+) {
+  const response = await api.post("/api/families", {
+    name,
+    description,
+    isPrivate,
+  });
+
+  return response.data;
+}
+
+export async function getMyFamilies() {
+  const response = await api.get("/api/families");
+
+  return response.data;
+}
+
+export async function getFamily(familyId: string) {
+  const response = await api.get(`/api/families/${familyId}`);
+
+  return response.data;
+}
+
+export default api;
